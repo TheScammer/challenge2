62945f74b4e9807e489180aea4d9f0e664b96129
Everett Higa <ehiga21@punahou.edu>
2016-07-07 14:21:08 -1000

    Worked on Perception adding text recognizers

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById('input_box').addEventListener('keyup',function (event){
+	var string =  document.getElementById("input_box").value
+
+	if string ==='noclickbutton'{
+		document.getElementById('link').href =10.3.54.100:8000
+	}
+});
+
